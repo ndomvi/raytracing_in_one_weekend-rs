a95086c8fcbe33de8db7b85da924af2c12eb9468
ndomvi <ndomvi@gmail.com>
2022-04-21 19:53:30 +0300

    improve dielectric reflectance (Schlick approx.)

--- src/materials/dielectric.rs
+++ src/materials/dielectric.rs
@@ -4,6 +4,7 @@ use crate::ray::Ray;
 use crate::Point;
 
 use rand::prelude::SmallRng;
+use rand::Rng;
 
 #[derive(Debug)]
 pub struct Dielectric {
@@ -16,10 +17,15 @@ impl Dielectric {
             ir: index_of_refraction,
         }
     }
+
+    fn reflectance(cosine: f32, refraction_ratio: f32) -> f32 {
+        let r0 = ((1.0 - refraction_ratio) / (1.0 + refraction_ratio)).powi(2);
+        r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
+    }
 }
 
 impl Material for Dielectric {
-    fn scatter(&self, ray: &Ray, rec: &HitRecord, _rng: &mut SmallRng) -> Option<(Ray, Point)> {
+    fn scatter(&self, ray: &Ray, rec: &HitRecord, rng: &mut SmallRng) -> Option<(Ray, Point)> {
         let refraction_ratio = if rec.front_face {
             1.0 / self.ir
         } else {
@@ -29,7 +35,9 @@ impl Material for Dielectric {
         let cos_theta = (-ray.direction.normalize()).dot(rec.normal).min(1.0);
         let sin_theta = (1.0 - cos_theta.powi(2)).sqrt();
 
-        let scattered = if refraction_ratio * sin_theta > 1.0 {
+        let scattered = if (refraction_ratio * sin_theta > 1.0)
+            || (Self::reflectance(cos_theta, refraction_ratio) > rng.gen::<f32>())
+        {
             ray.reflected(rec)
         } else {
             ray.refracted(rec, refraction_ratio)
